e4d4eed48cf4a860aa1acb4acafd8a4ebb10372b
saniok2525 <saniok2525@gmail.com>
2019-11-20 16:26:06 +0000

    Online-Shop

--- js/product-list.js
+++ js/product-list.js
@@ -4,6 +4,7 @@ class ProductList {
     fetch(productsUrl)
       .then(result => result.json())
       .then(products => {
+        products.sort( (a, b) => a.price - b.price );
         this.products = products;
         this.renderProducts(renderContainer, products);
         this.addEventListeners();
